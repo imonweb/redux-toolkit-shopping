dc1b54d4dcfc3a3ba7c95b9f645f086cac826b16
Imon <contact@imonweb.co.uk>
2024-03-19 01:08:35 +0000

    product

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import logo from './logo.svg';
 // import './App.css';
+import { BrowserRouter, Routes, Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Product from './components/Product';
+import Dashboard from './components/Dashboard'
+import Cart from './components/Cart'
+import RootLayout from './components/RootLayout';
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function App() {
+
+  const router = createBrowserRouter(createRoutesFromElements(
+  // <Route path="/" element={<RouteLayout />}>
+  //   <Route index element={<Dashboard />}></Route>
+  //   <Route path="/cart" element={<Cart />}></Route>
+  // </Route>
+  <Route path="/" element={<RootLayout />}>
+     <Route index element={<Dashboard />}></Route>
+     <Route path="/cart" element={<Cart />}></Route>
+  </Route>
+ 
+  ))
+
   return (
-    <div className="App">
-      <Product />
-    </div>
+    <Container fluid>
+      {/* <Product /> */}
+      <RouterProvider router={router} />
+    </Container>
   );
 }
 
---
+++ src/components/NavBarPanel.js
@@ -0,0 +1,39 @@
+// import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+// import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+import {Link} from 'react-router-dom'
+
+const NavBar = () => {
+  return (
+     <Navbar expand="lg" className="bg-body-tertiary">
+      <Container fluid>
+        <Navbar.Brand href="#">Redux Toolkit</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+          <Nav>
+            <Nav.Link to="/" as={Link}>Products</Nav.Link>
+          </Nav>
+          <Navbar.Toggle />
+          <Navbar.Collapse className='justify-content-end'>
+            <Navbar.Text>
+              <Nav.Link to="/cart" as={Link}>My Bag 0</Nav.Link>
+            </Navbar.Text>
+          </Navbar.Collapse>
+          {/* <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form> */}
+        
+      </Container>
+    </Navbar>
+  )
+}
+
+export default NavBar
